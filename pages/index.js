b0a9bf638c3231eaf12da9b54307e12e744390ef
Bruno Escritorio <carecsnay1@gmail.com>
2024-07-23 15:03:07 -0300

    Fix: First image and margin top

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="container mx-auto mt-5 rounded-lg bg-gray-300 p-5">
+    <main className="container mx-auto rounded-lg bg-gray-300 p-5 sm:mt-5">
       <div className="display grid h-[720px] p-3 sm:h-96 sm:grid-cols-2">
-        <div className="mb-2 mr-2 rounded-md bg-[url('/src/01.webp')] bg-cover bg-center sm:row-start-1 sm:row-end-3"></div>
+        <div className="mb-2 rounded-md bg-[url('/src/01.webp')] bg-cover bg-center sm:row-start-1 sm:row-end-3 sm:mr-2"></div>
         <div className="mb-2 rounded-md bg-[url('/src/02.webp')] bg-cover bg-center"></div>
         <div className="mb-2 rounded-md bg-[url('/src/03.webp')] bg-cover bg-center"></div>
       </div>
