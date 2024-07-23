cd2dfaff98dff8960015cc3d3f30d5f7564528ea
Bruno Escritorio <carecsnay1@gmail.com>
2024-07-23 14:53:29 -0300

    AT03 - Grid Responsivo (Ok)

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto mt-5 rounded-lg bg-gray-300 p-5">
+      <div className="display grid h-[720px] p-3 sm:h-96 sm:grid-cols-2">
+        <div className="mb-2 mr-2 rounded-md bg-[url('/src/01.webp')] bg-cover bg-center sm:row-start-1 sm:row-end-3"></div>
+        <div className="mb-2 rounded-md bg-[url('/src/02.webp')] bg-cover bg-center"></div>
+        <div className="mb-2 rounded-md bg-[url('/src/03.webp')] bg-cover bg-center"></div>
+      </div>
+      <div className="p-3 text-xl font-bold">Título da casa</div>
+      <div className="flex p-3">
+        <div className="mr-2 rounded-2xl bg-green-900 px-3 py-1 text-white hover:cursor-pointer hover:bg-green-600">
+          Campo
+        </div>
+        <div className="mr-2 rounded-2xl bg-green-900 px-3 py-1 text-white hover:cursor-pointer hover:bg-green-600">
+          Moderno
+        </div>
+        <div className="rounded-2xl bg-green-900 px-3 py-1 text-white hover:cursor-pointer hover:bg-green-600">
+          Piscina
+        </div>
+      </div>
+    </main>
+  );
+}
